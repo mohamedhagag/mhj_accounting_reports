40a2e7ca85de7d53604312e4254772c78f79b778
Your Name <you@example.com>
2025-10-01 00:48:06 +0300

    ffffff

--- accounting_pdf_reports/report/report_partner_ledger.py
+++ accounting_pdf_reports/report/report_partner_ledger.py
@@ -133,7 +133,7 @@ class ReportPartnerLedger(models.AbstractModel):
         result = 0.0
         AML = self.env['account.move.line']
         ctx = data['form'].get('used_context', {})
-        ctx['date_to'] = None  # Avoid date filtering for sum
+        ctx['date_from'] = None  # Avoid date filtering for sum
         query_get_data = AML.with_context(ctx)._query_get()
         reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".full_reconcile_id IS NULL '
 
