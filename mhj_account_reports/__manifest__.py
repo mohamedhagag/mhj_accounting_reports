59bfe559ece1c195a114bd1763e6386cea7569e0
Your Name <you@example.com>
2025-10-14 02:16:30 +0300

    up

--- mhj_account_reports/__manifest__.py
+++ mhj_account_reports/__manifest__.py
@@ -37,7 +37,7 @@
         "report/report_aged_partner.xml",
         "report/report_journal_audit.xml",
         "report/report_journal_entries.xml",
-        "views/optimization_tools.xml",
+        # "views/optimization_tools.xml",
     ],
     "external_dependencies": {
         "python": ["psutil", "xlsxwriter"],
