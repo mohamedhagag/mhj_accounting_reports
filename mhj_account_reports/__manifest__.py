23b199f5c7be3ca7e04da7c10e59a18c7fa35246
Your Name <you@example.com>
2025-12-10 08:10:01 +0200

    Fix P&L report not loading - add profit_loss_report.js to manifest assets
    The P&L component was created but not registered in the manifest's
    web.assets_backend, preventing Odoo from loading it. This caused
    'KeyNotFoundError: Cannot find key mhj_profit_loss_interactive'
    when trying to access the interactive P&L report.
    
    Solution: Added profit_loss_report.js to the assets list in __manifest__.py

--- mhj_account_reports/__manifest__.py
+++ mhj_account_reports/__manifest__.py
@@ -57,6 +57,7 @@
             "mhj_account_reports/static/src/js/partner_ledger_report.js",
             "mhj_account_reports/static/src/js/cash_flow_report.js",
             "mhj_account_reports/static/src/js/balance_sheet_report.js",
+            "mhj_account_reports/static/src/js/profit_loss_report.js",
             # XML Templates
             "mhj_account_reports/static/src/xml/financial_reports_templates.xml",
             # Styles
